e36ab8587d3f7fcb77f685b157c78bfa3d07fb60
Cactus <ysjungman@gmail.com>
2023-03-10 03:55:30 +0900

    유데미 리액트 초급

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Expense from './components/Expenses/Expenses';
+
+function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2021, 7, 14)
+    },
+    {
+      id: 'e2',
+      title: 'New Tv',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'New Desk(Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12)
+    },
+  ];
+
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Let's get started!"),
+    React.createElement(Expense, { items: expenses })
+  )
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expense items={expenses}/>
+  //   </div>
+  // );
+}
+
+export default App;
+
+
+
