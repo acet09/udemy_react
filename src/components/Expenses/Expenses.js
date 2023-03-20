895efc02e32b585bbd2e20411933f01539b62846
acet09 <ysjungman@gmail.com>
2023-03-21 03:29:58 +0900

    75.조건부 내용 (3가지)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,20 +16,61 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
+
   return (
     <div>
       <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {filteredExpenses.map(expense =>
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />)}        
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expensesContent}
       </Card>
     </div>
   );
 };
 
+
+
+
 export default Expenses;
+
+
+// ***type_1
+// {filteredExpenses.length === 0 ?
+//   (<p>No expenses found.</p>
+//   ) : (
+//     filteredExpenses.map((expense) => (
+//       <ExpenseItem
+//         key={expense.id}
+//         title={expense.title}
+//         amount={expense.amount}
+//         date={expense.date}
+//       />
+//     ))
+//   )}
+
+// ***type_2
+// {filteredExpenses.length === 0 && <p>No expenses found.</p>}
+//         {filteredExpenses.length > 0 &&
+//           filteredExpenses.map((expense) => (
+//               <ExpenseItem
+//                 key={expense.id}
+//                 title={expense.title}
+//                 amount={expense.amount}
+//                 date={expense.date}
+//               />
+//             ))}
