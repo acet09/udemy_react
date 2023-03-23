e981ffe7b8b3e898b37a2e3c13d0a2e2f56e31b8
acet09 <ysjungman@gmail.com>
2023-03-24 02:20:00 +0900

    80_마무리

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -23,13 +24,15 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList items={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
 };
 
 export default Expenses;
+
 // ***type_1
 // {filteredExpenses.length === 0 ?
 //   (<p>No expenses found.</p>
